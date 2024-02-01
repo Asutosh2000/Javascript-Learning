e3768779c8071cdfa045a9ab8b9414b236f3a970
Asutosh <asutosh@iterator.in>
2024-02-01 16:06:02 +0530

    updated fetch and ajax

---
+++ 10_advance_one/callbacks.js
@@ -0,0 +1,22 @@
+const posts = [
+  { title: "Post One", body: "this is post one" },
+  { title: "Post Two", body: "this is post two" },
+];
+function getPosts() {
+  setTimeout(() => {
+    let output = "";
+    posts.forEach((post, i) => {
+      output += `<li>${post.title}</li>`;
+    });
+    document.body.innerHTML = output;
+  }, 1000);
+}
+
+function createPost(post, callback) {
+  setTimeout(() => {
+    posts.push(post);
+    callback();
+  }, 2000);
+}
+
+createPost({ title: "Post Three", body: "This is post three" }, getPosts);
